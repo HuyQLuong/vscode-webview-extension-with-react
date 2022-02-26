d74be64729c984ee9ae6e0b2b5ecfb44e4ab782b
huyqluong <luongquochuy1995@gmail.com>
2022-02-26 16:59:20 +0700

    register webview provider when init extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,32 @@
-import * as vscode from 'vscode';
+try {
+	require("module-alias/register");
+} catch (e) {
+	console.log("module-alias import error !");
+}
+import * as vscode from "vscode";
+import { EXTENSION_CONSTANT } from "constant";
+import { LeftPanelWebview } from "providers/left-webview-provider";
 
 export function activate(context: vscode.ExtensionContext) {
+	let helloWorldCommand = vscode.commands.registerCommand(
+		"vscode-webview-extension-with-react.helloWorld",
+		() => {
+			vscode.window.showInformationMessage(
+				"Hello World from vscode-webview-extension-with-react!"
+			);
+		}
+	);
+	context.subscriptions.push(helloWorldCommand);
 
-	let disposable = vscode.commands.registerCommand('vscode-webview-extension-with-react.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from vscode-webview-extension-with-react!');
-	});
+	// Register view
+	const leftPanelWebViewProvider = new LeftPanelWebview(context?.extensionUri, {});
+	let view = vscode.window.registerWebviewViewProvider(
+		EXTENSION_CONSTANT.LEFT_PANEL_WEBVIEW_ID,
+		leftPanelWebViewProvider,
+	);
+	context.subscriptions.push(view);
 
-	context.subscriptions.push(disposable);
-}
+};
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
