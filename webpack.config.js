3b126251360831f94ac7d93ff407ecc02329c28d
huyqluong <luongquochuy1995@gmail.com>
2022-02-26 16:55:50 +0700

    setup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,41 @@
 'use strict';
 
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-//@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-/** @type WebpackConfig */
-const extensionConfig = {
+/**@type {import('webpack').Configuration}*/
+// @ts-ignore
+const config = {
+
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dir'),
     filename: 'extension.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
   },
+  devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.tsx'],
+    alias: {
+      utils: path.resolve(__dirname, './src/utils.ts'),
+      constant: path.resolve(__dirname, './src/constant.ts'),
+      providers: path.resolve(__dirname, './src/providers/'),
+      components: path.resolve(__dirname, './src/components/'),
+    },
   },
+  plugins: [
+      // @ts-ignore
+      new Dotenv()
+  ],
   module: {
     rules: [
       {
@@ -37,12 +48,22 @@ const extensionConfig = {
             loader: 'ts-loader'
           }
         ]
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [ 'style-loader', 'css-loader']
       }
     ]
-  },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
-  },
+  }
 };
-module.exports = [ extensionConfig ];
+module.exports = config;
